a22d11d31110f11807b721d9758fa3fc0aa6f72b
ionvop <43801264+ionvop@users.noreply.github.com>
2024-02-21 00:40:30 +0800

    Add files via upload

---
+++ v1/chat/completions/index.php
@@ -0,0 +1,117 @@
+<?php
+
+$headers = getallheaders();
+$apiKey = explode("Bearer ", $headers["Authorization"])[1];
+
+if ($headers["Content-Type"] == "application/json") {
+    $_POST = json_decode(file_get_contents("php://input"), true);
+}
+
+$previousRole = "user";
+$context = "";
+$history = [];
+
+$message = [
+    "input" => "",
+    "response" => ""
+];
+
+foreach ($_POST["messages"] as $key => $value) {
+    switch ($value["role"]) {
+        case "system":
+            $context = $value["content"];
+            break;
+        case "user":
+            if ($previousRole == "assistant") {
+                $history[] = $message;
+                
+                $message = [
+                    "input" => "",
+                    "response" => ""
+                ];
+            }
+
+            $message["input"] .= $value["content"];
+            $previousRole = "user";
+            break;
+        case "assistant":
+            $message["response"] .= $value["content"];
+            $previousRole = "assistant";
+            break;
+        default:
+            http_response_code(400);
+
+            $result = [
+                "error" => [
+                    "code" => "invalid_role",
+                    "message" => "Invalid role"
+                ]
+            ];
+
+            exit(json_encode($result));
+    }
+}
+
+$reqHeaders = [
+    "Content-Type: application/json",
+    "Authorization: Token " . $apiKey
+];
+
+$reqData = [
+    "input" => $message["input"],
+    "context" => $context,
+    "history" => $history
+];
+
+$reqData = json_encode($reqData);
+$res = SendCurl("https://api.nlpcloud.io/v1/gpu/" . $_POST["model"] . "/chatbot", "POST", $reqHeaders, $reqData);
+$res = json_decode($res, true);
+
+$result = [
+    "id" => uniqid("chatcmpl-"),
+    "object" => "chat.completion",
+    "created" => time(),
+    "model" => $_POST["model"],
+    "system_fingerprint" => null,
+    "choices" => [
+        [
+            "index" => 0,
+            "message" => [
+                "role" => "assistant",
+                "content" => $res["response"]
+            ],
+            "logprobs" => null,
+            "finish_reason" => "stop"
+        ]
+    ],
+    "usage" => [
+        "prompt_tokens" => null,
+        "completion_tokens" => null,
+        "total_tokens" => null
+    ]
+];
+
+exit(json_encode($result));
+
+function SendCurl($url, $method, $headers, $data) {
+    $ch = curl_init();
+    
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+
+    curl_close($ch);
+    return $result;
+}
+
+?>
